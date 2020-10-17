4a560e408ce3a2e89f9e24c02e0ba64f6a609114
daeyoungkoh12345 <72638510+daeyoungkoh12345@users.noreply.github.com>
2020-10-17 12:07:37 +0900

    Add files via upload

---
+++ 20200114_input.py
@@ -0,0 +1,20 @@
+a=int(input(" :) "))          #입력함수
+b=int(input(" :( "))
+print("a값은:",a)              #출력함수
+print("b값은:",b)
+print("사칙연산 값은?")
+print("a더하기b" ,a+b)
+print("a빼기b" ,a-b)
+print("a곱하기b" ,a*b)
+print("a나누기b" ,a/b)      #나머지값이 다 나오는 경우
+
+print(a%b)                     #나머지값
+print(a//b)                     #몫
+print(a**b)                    #지수
+
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)                   #같다
+print(a!=b)                    #같지 않다
